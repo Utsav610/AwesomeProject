acdcdcc4ea37a2fd777ad60db4d42362a98217af
Aditi Garg <aditigarg201@gmail.com>
2026-01-30 12:51:28 +0530

    refactor: update import paths to use index files for better organization

--- App.tsx
+++ App.tsx
@@ -2,12 +2,11 @@ import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { QueryClientProvider } from '@tanstack/react-query';
 
-import { queryClient } from '@queries/queryClient';
-import { RootNavigator } from '@navigation/RootNavigator';
+import { queryClient } from '@queries/index';
+import { RootNavigator } from '@navigation/index';
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import { CommonSafeAreaView } from '@components/base';
-import { Text } from '@components/base';
+import { CommonSafeAreaView, Text } from '@components/base';
 import { getStyles } from './src/styles/getStyles';
 
 export default function App() {
---
+++ src/navigation/index.ts
@@ -0,0 +1,5 @@
+export { AppNavigator } from './AppNavigator';
+export { AuthNavigator } from './AuthNavigator';
+export { RootNavigator } from './RootNavigator';
+export * from './ScreenNames';
+export * from './types';
