7c11378cb6665cd2eeccb6fa78aa3c9cdb90c669
Aditi Garg <aditigarg201@gmail.com>
2026-02-04 14:39:04 +0530

    added segmentedcontrol components

--- src/components/composite/index.ts
+++ src/components/composite/index.ts
@@ -1,3 +1,5 @@
 export { Toggle } from './Toggle';
 export { BuySellFooter } from './BuySellFooter';
 export { SelectionTab } from './SelectionTab';
+export { Header } from './Header';
+export { SegmentedControl } from './SegmentedControl';
