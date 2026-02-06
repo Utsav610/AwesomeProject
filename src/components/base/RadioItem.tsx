1c82cf8f432efc87bb18562a9238df52aaf19f64
Aditi Garg <aditigarg201@gmail.com>
2026-02-06 10:38:04 +0530

    added optional sublabel in radio item

--- src/components/base/RadioItem.tsx
+++ src/components/base/RadioItem.tsx
@@ -1,24 +1,34 @@
 import { FC } from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { Text } from '@components/base';
 import { getStyles } from '@/styles';
 import { Radio, SelectRadio } from '@/assets/icons';
 
 interface RadioItemProps {
   label: string;
+  subLabel?: string;
   selected: boolean;
   onPress: () => void;
 }
 
-export const RadioItem: FC<RadioItemProps> = ({ label, selected, onPress }) => {
+export const RadioItem: FC<RadioItemProps> = ({ label, subLabel, selected, onPress }) => {
   return (
     <TouchableOpacity
       style={getStyles('flex-row px-16 py-12 justify-between items-center')}
       onPress={onPress}
+      activeOpacity={0.7}
     >
-      <Text >{label}</Text>
-      {selected ? <SelectRadio /> : <Radio />}
+      <View style={getStyles('flex-1')}>
+        <Text>{label}</Text>
 
+        {subLabel ? (
+          <Text size="sm" color="textSecondary" style={getStyles('mt-2')}>
+            {subLabel}
+          </Text>
+        ) : null}
+      </View>
+
+      {selected ? <SelectRadio /> : <Radio />}
     </TouchableOpacity>
   );
 };
